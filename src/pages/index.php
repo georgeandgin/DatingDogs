eb10969e2d6508e6280882c6cf36146c3a83ada7
Gintare Kunigonyte <gintare.kunigonyte@gmail.com>
2021-05-31 01:08:47 +0300

    php

--- src/pages/index.php
+++ src/pages/index.php
@@ -7,6 +7,21 @@
     <title>Dating Dogs</title>
 </head>
 <body>
-    
+
+<?php include ('header.php');?>
+<?php
+    $query = "SELECT * FROM user"
+    $stmt = $db->prepare($query);
+    $stmt->bind_result($username);
+    $stmt->execute();
+
+    while ($stmt->fetch()) {
+        echo "<p>$username</p>";
+      }
+
+      $stmt->close();
+
+?>    
+
 </body>
 </html>
