f425ab6c5f145e0b566161baaeeb857bfd96ee95
Gintare Kunigonyte <gintare.kunigonyte@gmail.com>
2021-06-02 18:15:16 +0200

    register

--- src/components/register.php
+++ src/components/register.php
@@ -5,16 +5,30 @@
 
 <?php include ('header.php');?>
 
-<form action="" method="POST">
+<form action="register2.php" method="POST">
     <label for="username">Username:</label>
     <input type="text"id="username" name="username" value=""><br>
+    <label for="email">Email:</label>
+    <input type="text" id="pemail" name="email" value=""><br>
     <label for="password">Password:</label>
     <input type="password" id="password" name="password" value=""><br>
     <input type="submit" name="submit" value="Next" onclick="">
 </form>
 
 <?php
- 
+    // save input values
+    if (isset($_POST['submit'])) {
+        $username = mysqli_real_escape_string($db, $_POST['username']);
+        $password = mysqli_real_escape_string($db, $_POST['email']);
+        $email = mysqli_real_escape_string($db, $_POST['email']);
+    }
+
+    // make sure all the fields are filled in
+    if (empty($username)) { array_push($errors, "Username is required"); }
+    if (empty($email)) { array_push($errors, "Email is required"); }
+    if (empty($password)) { array_push($errors, "Password is required"); }
+
+    
 ?>
 
 </body>
