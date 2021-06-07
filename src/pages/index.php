e87d2af99b8ddda8b63596847afc97b21041cddd
Gintare Kunigonyte <gintare.kunigonyte@gmail.com>
2021-06-07 15:42:32 +0200

    idk

--- src/pages/index.php
+++ src/pages/index.php
@@ -16,12 +16,12 @@
 <div class="both">
 <div class="options">
 <a href="forum.php"><img id="forumPic" src="../assets/forumPic.png" alt="DOG BONE"></a>
-<h3>Forum</h3>
+<h3><a href="forum.php">Forum</a></h3>
 </div>
 
 <div class="options">
 <a href="../pages/profiles.php"><img id="profilePic" src="../assets/profilesPic.png" alt="DOG BONE"></a>
-<h3>Profiles</h3>
+<h3><a href="profiles.php">Profiles</a></h3>
 </div>
 </div>
 <?php include ('../components/footer.php');?>
