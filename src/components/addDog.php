7405c89d723afae99be3199149828955c5468b31
Gintare Kunigonyte <gintare.kunigonyte@gmail.com>
2021-06-08 11:48:52 +0200

    dog filtering

---
+++ src/components/addDog.php
@@ -0,0 +1,72 @@
+<?php include ('../components/config.php');?>
+<?php include ('../components/connect.php');?>
+
+<body>
+
+<?php include ('header.php');?>
+
+<form action="" method="POST" enctype="multipart/form-data">
+    <label for="dogName">Dog's name:</label>
+    <input type="text"id="dogName" name="dogName" required><br>
+    <label for="breed">Breed:</label>
+    <input type="text" id="breed" name="breed" required><br>
+    <label for="mating">Mating:</label>
+    <input type="radio" id="mating" name="mating" value="no" required>
+    <label for="yes">YES</label><br>
+    <input type="radio" id="mating" name="mating" value="yes" required>
+    <label for="no">NO</label><br>
+    <label for="breed">Location:</label>
+    <input type="text" id="location" name="location" required><br>
+    <label for="breed">Description:</label>
+    <input type="text" id="description" name="description" required><br>
+    <label for="breed">Phone number:</label>
+    <input type="text" id="phone" name="phone" required><br>
+    <label for="image">Profile picture:</label>
+    <input type="file" id="image" name="image" accept="image/*" required><br>
+    <input type="submit" name="submit" value="Register dog profile" onclick="">
+</form>
+
+<?php
+    if (isset($_POST['submit'])){
+        $dogName = $_POST['dogName'];
+        $breed = $_POST['breed'];
+        $answer = $_POST['mating'];  
+        if ($answer == "yes") {          
+            $mating = 'Yes';  
+        }
+        else {
+            $mating = 'No'; 
+        }   
+        $location = $_POST['location'];
+        $description = $_POST['description'];
+        $phoneNumber = $_POST['phone'];
+
+        $user = $_SESSION['username'];
+
+        $query = "SELECT userID from user where username = ?";
+        $stmt = $db->prepare($query);
+        $stmt->bind_param('s', $user);
+        $stmt->execute();
+        $stmt->bind_result($userID);
+        $stmt->fetch();
+        $stmt->close();
+
+        $_SESSION["userID"] = $userID;
+        $owner = $userID;
+
+        $profilePicture = $_FILES['image']['name'];
+	
+	    move_uploaded_file($_FILES["image"]["tmp_name"],'../profileImg/'.$profilePicture);
+
+        $query = "INSERT INTO dog (dogName, breed, mating, location, description, phoneNumber, profilePicture, owner) VALUES (?,?,?,?,?,?,?,?)";
+
+        $stmt = $db->prepare($query);
+        $stmt->bind_param('sssssssi', $dogName, $breed, $mating, $location, $description, $phoneNumber, $profilePicture, $owner);
+        $stmt->execute();
+        $stmt->close();
+
+        header('Location: ../pages/index.php');
+    } 
+?>
+
+</body>
--- src/pages/profiles.php
+++ src/pages/profiles.php
@@ -14,8 +14,106 @@
 <?php include ('../components/header.php');?>
 
 <div class="container">
-    <h2> Search by breed: ____________<h2>
+    <h2> Filter by breed: <h2>
+    <form action="" method="POST" id="breedForm">
+        <select name="breeds" id="breed">
+        <option name="breed" value="Australian Shepherd">Australian Shepherd</option>
+        <option name="breed" value="Poodle">Poodle</option>
+        <option name="breed" value="Pug">Pug</option>
+        <option name="breed" value="Komondor">Komondor</option>
+        <option name="breed" value="Labrador">Labrador</option>
+        <option name="breed" value="Golden doodle">Golden doodle</option>
+        <input  class="" type="submit" name="submit" value="submit">
+    </form>
+    </select>
+
+    <?php
+        if(isset($_POST['submit'])) {
+            if($_POST['breeds']=='Australian Shepherd') {  
+                $query = "SELECT dogID, dogName, breed, profilePicture from dog WHERE breed = 'Australian Shepherd'";
+
+                $stmt = $db->prepare($query);
+                $stmt->bind_result($dogID, $dogName, $breed, $profilePicture);
+                $stmt->execute();
+        
+                while ($stmt->fetch()) {
+                    echo "<div class='item'>";
+                    echo "<a href=\"profile.php?dogID=$dogID\"><img src='../profileImg/" . $profilePicture . "'/></a>";
+                    echo "<h4><a href=\"profile.php?dogID=$dogID\">$dogName</a></h4>";
+                    echo "</div>"; 
+                }
+          
+                $stmt->close();
+            }
+            elseif($_POST['breeds']=='Poodle') { 
+                $query = "SELECT dogID, dogName, breed, profilePicture from dog WHERE breed = 'Poodle'";
+
+                $stmt = $db->prepare($query);
+                $stmt->bind_result($dogID, $dogName, $breed, $profilePicture);
+                $stmt->execute();
+        
+                while ($stmt->fetch()) {
+                    echo "<div class='item'>";
+                    echo "<a href=\"profile.php?dogID=$dogID\"><img src='../profileImg/" . $profilePicture . "'/></a>";
+                    echo "<h4><a href=\"profile.php?dogID=$dogID\">$dogName</a></h4>";
+                    echo "</div>"; 
+                }
+          
+                $stmt->close();
+            } elseif($_POST['breeds']=='Pug') { 
+                $query = "SELECT dogID, dogName, breed, profilePicture from dog WHERE breed = 'Pug'";
+
+                $stmt = $db->prepare($query);
+                $stmt->bind_result($dogID, $dogName, $breed, $profilePicture);
+                $stmt->execute();
+        
+                while ($stmt->fetch()) {
+                    echo "<div class='item'>";
+                    echo "<a href=\"profile.php?dogID=$dogID\"><img src='../profileImg/" . $profilePicture . "'/></a>";
+                    echo "<h4><a href=\"profile.php?dogID=$dogID\">$dogName</a></h4>";
+                    echo "</div>"; 
+                }
+          
+                $stmt->close();
+            } elseif($_POST['breeds']=='Komondor') { 
+                $query = "SELECT dogID, dogName, breed, profilePicture from dog WHERE breed = 'Komondor'";
+
+                $stmt = $db->prepare($query);
+                $stmt->bind_result($dogID, $dogName, $breed, $profilePicture);
+                $stmt->execute();
+        
+                while ($stmt->fetch()) {
+                    echo "<div class='item'>";
+                    echo "<a href=\"profile.php?dogID=$dogID\"><img src='../profileImg/" . $profilePicture . "'/></a>";
+                    echo "<h4><a href=\"profile.php?dogID=$dogID\">$dogName</a></h4>";
+                    echo "</div>"; 
+                }
+          
+                $stmt->close();
+            } elseif($_POST['breeds']=='Labrador') { 
+                $query = "SELECT dogID, dogName, breed, profilePicture from dog WHERE breed = 'Labrador'";
+
+                $stmt = $db->prepare($query);
+                $stmt->bind_result($dogID, $dogName, $breed, $profilePicture);
+                $stmt->execute();
+        
+                while ($stmt->fetch()) {
+                    echo "<div class='item'>";
+                    echo "<a href=\"profile.php?dogID=$dogID\"><img src='../profileImg/" . $profilePicture . "'/></a>";
+                    echo "<h4><a href=\"profile.php?dogID=$dogID\">$dogName</a></h4>";
+                    echo "</div>"; 
+                }
+          
+                $stmt->close();
+            }
+            
+          //$results = mysqli_query($dblink, $sql) or die (mysqli_error());
+          }
+
+    ?>
+
 <div class="profiles">
+    <h2>All dogs</h2>
     <?php
         $query = "SELECT dogID, dogName, profilePicture from dog";
 
