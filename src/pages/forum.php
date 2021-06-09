d3931fef558b87eb7603e51009fa08c912f9a327
Gintare Kunigonyte <gintare.kunigonyte@gmail.com>
2021-06-09 12:36:40 +0200

    d

--- src/pages/forum.php
+++ src/pages/forum.php
@@ -23,27 +23,26 @@
 </div>
 
 <?php
+    if(!empty($_SESSION['username'])){
+        echo "<h3 class='newpost'><a href='../components/createpost.php'>Create a post</a></h3>";
+    }
 
-        if(!empty($_SESSION['username'])){
-            echo "<h3 class='newpost'><a href='../components/createpost.php'>Create a post</a></h3>";
-        }
+    $query = "SELECT forumPost.postID, forumPost.userID, forumPost.heading, user.username FROM forumPost INNER JOIN user ON (forumPost.userID = user.userID)";
 
-        $query = "SELECT forumPost.postID, forumPost.userID, forumPost.heading, user.username FROM forumPost INNER JOIN user ON (forumPost.userID = user.userID)";
+    $stmt = $db->prepare($query);
+    $stmt->bind_result($postID, $userID, $heading, $username);
+    $stmt->execute();
 
-        $stmt = $db->prepare($query);
-        $stmt->bind_result($postID, $userID, $heading, $username);
-        $stmt->execute();
-
-        while ($stmt->fetch()) {
-            echo "<div class='forumpost'>";
-            echo "<a href=\"insidePost.php?postID=$postID\">$heading</a>";
-            echo "<img src='../assets/bone.png'>";
-            echo "<div class='break'></div>";
-            echo "<h6> By $username</h6>";
-            echo "</div>"; 
-        }
+    while ($stmt->fetch()) {
+        echo "<div class='forumpost'>";
+        echo "<a href=\"insidePost.php?postID=$postID\">$heading</a>";
+        echo "<img src='../assets/bone.png'>";
+        echo "<div class='break'></div>";
+        echo "<h6> By $username</h6>";
+        echo "</div>"; 
+    }
   
-        $stmt->close();
+    $stmt->close();
 ?>
 
 </div>
