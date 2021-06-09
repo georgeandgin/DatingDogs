8a39a08bba18b8784da34c619574b4fbdbaf270f
Gintare Kunigonyte <gintare.kunigonyte@gmail.com>
2021-06-09 11:44:07 +0200

    s

--- src/components/createpost.php
+++ src/components/createpost.php
@@ -21,25 +21,6 @@
 <form action="" method="POST" id="usrform">
     <label for="heading">Heading</label>
     <input type="text" id="heading" name="heading" required>
-    <label for="tags">Select a tags</label>
-    <input type="checkbox" id="food" name="food">
-    <label for="food">food</label>
-    <input type="checkbox" id="health" name="health">
-    <label for="health">health</label>
-    <input type="checkbox" id="games" name="games">
-    <label for="games">games</label>
-    <input type="checkbox" id="sports" name="sports">
-    <label for="sports">sports</label>
-    <input type="checkbox" id="beauty" name="beauty">
-    <label for="beauty">beauty</label>
-    <input type="checkbox" id="training" name="training">
-    <label for="training">training</label>
-    <input type="checkbox" id="mating" name="mating">
-    <label for="mating">mating</label>
-    <input type="checkbox" id="fashion" name="fashion">
-    <label for="fashion">fashion</label>
-    <input type="checkbox" id="other" name="other">
-    <label for="other">other</label>
     <input type="text" id="text" name="text" required>
     <input class="next" type="submit" name="submit" value="Submit" onclick="">
 </form>
