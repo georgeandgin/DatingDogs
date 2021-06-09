8b9dfb8c2acfef8d84c704192457ac4260084be2
Gintare Kunigonyte <gintare.kunigonyte@gmail.com>
2021-06-09 11:28:20 +0200

    all

--- src/pages/insidePost.php
+++ src/pages/insidePost.php
@@ -38,13 +38,14 @@
 
 </div>
 
-<h2>Add comment</h2>
-<form action="" method="POST" id="comment">
-    <input type="text" id="text" name="text" required>
-    <input class="next" type="submit" name="submit" value="Submit" onclick="">
-</form>
-
 <?php
+    if(!empty($_SESSION['username'])){
+        echo "<form action='' method='POST' id='comment'>";
+        echo "<input type='text' id='text' name='text' required>";
+        echo "<input class='next' type='submit' name='submit' value='Submit' onclick=''>";
+        echo "</form>";
+    } 
+
     $query = "SELECT comment.commentID, comment.postID, comment.userID, comment.text, user.username FROM comment INNER JOIN user ON (comment.userID = user.userID) WHERE postID = $postID";
 
     $stmt = $db->prepare($query);
